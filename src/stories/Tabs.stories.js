0dac11d1b89fc9a8a7470958bbbb39b500c3f0a1
Florin Tudor <49878506+razer89@users.noreply.github.com>
2023-11-29 14:28:39 +0000

    update for vertical tabs

--- src/stories/Tabs.stories.js
+++ src/stories/Tabs.stories.js
@@ -117,3 +117,60 @@ BootstrapVerticalTabs.args = {
   isVertical: true,
   verticalClass: 'custom-vertical-tabs',
 };
+
+BootstrapVerticalTabs.parameters = {
+    docs: {
+      source: {
+        code: `
+          // HTML markup for the vertical tabs
+  
+          <div class="d-flex align-items-start">
+            <div class="nav flex-column nav-pills me-3" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+                <button class="nav-link active" id="v-pills-home-tab" data-bs-toggle="pill" data-bs-target="#v-pills-home" type="button" role="tab" aria-controls="v-pills-home" aria-selected="true">Home</button>
+                <button class="nav-link" id="v-pills-profile-tab" data-bs-toggle="pill" data-bs-target="#v-pills-profile" type="button" role="tab" aria-controls="v-pills-profile" aria-selected="false">Profile</button>
+                <button class="nav-link" id="v-pills-messages-tab" data-bs-toggle="pill" data-bs-target="#v-pills-messages" type="button" role="tab" aria-controls="v-pills-messages" aria-selected="false">Messages</button>
+                <button class="nav-link" id="v-pills-settings-tab" data-bs-toggle="pill" data-bs-target="#v-pills-settings" type="button" role="tab" aria-controls="v-pills-settings" aria-selected="false">Settings</button>
+            </div>
+            <div class="tab-content" id="v-pills-tabContent">
+                <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab">...</div>
+                <div class="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab">...</div>
+                <div class="tab-pane fade" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab">...</div>
+                <div class="tab-pane fade" id="v-pills-settings" role="tabpanel" aria-labelledby="v-pills-settings-tab">...</div>
+            </div>
+           </div>
+
+  
+          // CSS styles for the vertical tabs
+          .custom-vertical-tabs .v-content {
+            color: #090A0B !important;
+            font-family: 'Poppins';
+            font-weight: normal;
+            font-size: 14px;
+            word-wrap: break-word;
+            padding: 10px 15px 15px 15px;
+          }
+          
+          .custom-vertical-tabs .nav-link {
+            color: #393F41;
+            background-color: #FFF;
+            text-align: left;
+            padding: .75rem 1.25rem;
+            font-weight: 400;
+          }
+        
+          .custom-vertical-tabs .nav-link:hover {
+            color: #003594;
+            background-color: #F8F9FA;
+            border-color: #00153B;
+          }
+        
+          .custom-vertical-tabs .nav-link.active {
+            background-color: #003594 !important;
+            border-color: #003594 !important;
+            color: #FFF !important;
+            text-align: left;
+          }
+        `,
+      },
+    },
+  };
