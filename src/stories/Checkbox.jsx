340c5ce85dd4929a0aa5b4bda143c03809c2c195
Florin Tudor <49878506+razer89@users.noreply.github.com>
2024-01-04 12:52:51 +0000

    added checkboxes

---
+++ src/stories/Checkbox.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './checkbox.css';
+
+const Checkbox = ({ label, checked }) => {
+  return (
+    <div className="form-check">
+      <input
+        className="form-check-input"
+        type="checkbox"
+        id={`flexCheck${label}`}
+        checked={checked}
+      />
+      <label className="form-check-label" htmlFor={`flexCheck${label}`}>
+        {label}
+      </label>
+    </div>
+  );
+};
+
+export default Checkbox;
