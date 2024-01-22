585c0ccf82facd1384ef97065535a009c1bc33fe
Florin Tudor <49878506+razer89@users.noreply.github.com>
2024-01-22 10:28:03 +0000

    Adjusted Cards component to Bootstrap 4

--- src/stories/Card/Cards.jsx
+++ src/stories/Card/Cards.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './cards.less';
 
-export const Cards = ({ title, subtitle, text, link1, link2 }) => (
-  <div className="card" style={{ width: '18rem' }}>
-    <div className="card-body">
+export const Cards = ({ title, text, link1 }) => (
+  <div className="card1" style={{ width: '18rem' }}>
+    <div className="card-body-1">
       <h5 className="card-title">{title}</h5>
       <p className="card-text">{text}</p>
       <a href={link1.url} className="card-link">
