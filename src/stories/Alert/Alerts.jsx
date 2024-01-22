8bed5174264cfdb01b015fe8ec4dbf2507031550
Florin Tudor <49878506+razer89@users.noreply.github.com>
2024-01-22 10:48:45 +0000

    Adjusted Alert component to Bootstrap 4

--- src/stories/Alert/Alerts.jsx
+++ src/stories/Alert/Alerts.jsx
@@ -1,17 +1,15 @@
 import React from 'react';
-import 'bootstrap-icons/font/bootstrap-icons.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './alerts.less';
 
 const Alert = ({ type, message, additionalInfo }) => (
-    <div className={`alert alert-${type} alert-dismissible fade show d-flex align-items-center`} role="alert">
-      <div>
-        <div className='alert-content'>
-        <strong className="alert-strong">{message}</strong>
-        {additionalInfo && <p className="alert-paragraph">{additionalInfo}</p>}
-        </div>
-        <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-      </div>
-    </div>
-  );
+  <div className={`alert alert-${type} alert-dismissible fade show`} role="alert">
+    <strong className="alert-strong">{message}</strong>
+    {additionalInfo && <p className="alert-paragraph">{additionalInfo}</p>}
+    <button type="button" className="close" data-dismiss="alert" aria-label="Close">
+      <span aria-hidden="true">&times;</span>
+    </button>
+  </div>
+);
 
 export default Alert;
