bffeeb4a8287142e6402656aa5cf871497fe31c8
Florin Tudor <49878506+razer89@users.noreply.github.com>
2024-01-22 15:30:40 +0000

    Fixes

--- src/stories/Button/Button.stories.js
+++ src/stories/Button/Button.stories.js
@@ -17,11 +17,65 @@ export default {
 
 const Template = (args) => <Button {...args} />;
 
-export const Primary = Template.bind({});
-Primary.args = {
-  primary: true,
-  label: 'Primary Button', 'aria-label': 'Primary Button',
-};
+
+export const Primary = () => (
+  <div>
+    <div style={{ textAlign: 'center' }}>
+      <Button
+        primary
+        label="Primary Button"
+        aria-label="Primary Button"
+      />
+    </div>
+
+    <div className='usageguide' style={{ marginTop: '60px' }}>
+      <div className="s670">
+        <div data-hook="dodont-do" className="s671 o135---skin-5-green">
+          <p className="s896 o195---size-6-medium o195---weight-4-bold s672 o135---skin-5-green">Do</p>
+          <div className="s673">
+            <span className="s896 o195---size-6-medium o195---weight-6-normal s674">
+              <div className="s677">
+                <svg viewBox="0 0 18 18" fill="currentColor" width="18" height="18">
+                  <path d="M9,16 C5.13400675,16 2,12.8659932 2,9 C2,5.13400675 5.13400675,2 9,2 C12.8659932,2 16,5.13400675 16,9 C16,12.8659932 12.8659932,16 9,16 Z M10.9393398,7 L8.5,9.43933983 L7.06066017,8 C6.76776695,7.70710678 6.29289322,7.70710678 6,8 C5.70710678,8.29289322 5.70710678,8.76776695 6,9.06066017 L8.5,11.5606602 L12,8.06066017 C12.2928932,7.76776695 12.2928932,7.29289322 12,7 C11.7071068,6.70710678 11.232233,6.70710678 10.9393398,7 Z"></path>
+                </svg>
+              </div>
+              Use it to communicate primary and secondary actions users can take.
+            </span>
+            <span className="s896 o195---size-6-medium o195---weight-6-normal s674">
+              <div className="s677">
+                <svg viewBox="0 0 18 18" fill="currentColor" width="18" height="18">
+                  <path d="M9,16 C5.13400675,16 2,12.8659932 2,9 C2,5.13400675 5.13400675,2 9,2 C12.8659932,2 16,5.13400675 16,9 C16,12.8659932 12.8659932,16 9,16 Z M10.9393398,7 L8.5,9.43933983 L7.06066017,8 C6.76776695,7.70710678 6.29289322,7.70710678 6,8 C5.70710678,8.29289322 5.70710678,8.76776695 6,9.06066017 L8.5,11.5606602 L12,8.06066017 C12.2928932,7.76776695 12.2928932,7.29289322 12,7 C11.7071068,6.70710678 11.232233,6.70710678 10.9393398,7 Z"></path>
+                </svg>
+              </div>
+              The .btn classes are designed to be used with the &lt;button&gt; element.<br></br> However, you can also use these classes on &lt;a&gt; or &lt;input&gt; elements (though some browsers may apply a slightly different rendering).
+            </span>
+          </div>
+        </div>
+        <div data-hook="dodont-dont" className="s671 o135---skin-3-red">
+          <p className="s896 o195---size-6-medium o195---weight-4-bold s672 o135---skin-3-red">Don't</p>
+          <div className="s673">
+            <span className="s896 o195---size-6-medium o195---weight-6-normal s674">
+              <div className="s675">
+                <svg viewBox="0 0 18 18" fill="currentColor" width="18" height="18" className="s676">
+                  <path d="M8.43933983,9.5 L6,7.06066017 C5.70710678,6.76776695 5.70710678,6.29289322 6,6 C6.29289322,5.70710678 6.76776695,5.70710678 7.06066017,6 L9.5,8.43933983 L11.9393398,6 C12.232233,5.70710678 12.7071068,5.70710678 13,6 C13.2928932,6.29289322 13.2928932,6.76776695 13,7.06066017 L10.5606602,9.5 L13,11.9393398 C13.2928932,12.232233 13.2928932,12.7071068 13,13 C12.7071068,13.2928932 12.232233,13.2928932 11.9393398,13 L9.5,10.5606602 L7.06066017,13 C6.76776695,13.2928932 6.29289322,13.2928932 6,13 C5.70710678,12.7071068 5.70710678,12.232233 6,11.9393398 L8.43933983,9.5 Z"></path>
+                </svg>
+              </div>
+              Don't use buttons for non-interactive content.
+            </span>
+            <span className="s896 o195---size-6-medium o195---weight-6-normal s674">
+              <div className="s675">
+                <svg viewBox="0 0 18 18" fill="currentColor" width="18" height="18" className="s676">
+                  <path d="M8.43933983,9.5 L6,7.06066017 C5.70710678,6.76776695 5.70710678,6.29289322 6,6 C6.29289322,5.70710678 6.76776695,5.70710678 7.06066017,6 L9.5,8.43933983 L11.9393398,6 C12.232233,5.70710678 12.7071068,5.70710678 13,6 C13.2928932,6.29289322 13.2928932,6.76776695 13,7.06066017 L10.5606602,9.5 L13,11.9393398 C13.2928932,12.232233 13.2928932,12.7071068 13,13 C12.7071068,13.2928932 12.232233,13.2928932 11.9393398,13 L9.5,10.5606602 L7.06066017,13 C6.76776695,13.2928932 6.29289322,13.2928932 6,13 C5.70710678,12.7071068 5.70710678,12.232233 6,11.9393398 L8.43933983,9.5 Z"></path>
+                </svg>
+              </div>
+              Don’t use more than one primary button at a time.
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+);
 
 // Source code explanation for Primary Button Story
 Primary.storyName = 'Primary Button';
